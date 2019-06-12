2510499514d1cb08f5d22797e6cbaa2f8d1a88e3
Sheena Brevig <sheena.brevig@gmail.com>
2019-06-12 09:32:40 -0700

    first steps

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+//create general page components
+alert('Are you ready to play!?')
+//press any key to start
+
+//comp needs to pick random word 
+
+//display of letter placeholders = to the number of letters in word chosen
+
+//when keyUp(?) selected key is compared to letters in randomWord and
+
+    //IF letter is true or ===, that letter replaces the placeholder.
+    //ELSE then placed under as letters already guessed.
+
+//Define number of max guesses.    
+
+    //If word is guessed before maxGuesses then .textContent YOU WIN! then game reset.
+
+    //If max guesses reached, then .textContext LOSER. Game reset. 
