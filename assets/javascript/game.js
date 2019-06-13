2a53e70d5c5ea8463934369a7a69bae3059bb6e9
Sheena Brevig <sheena.brevig@gmail.com>
2019-06-12 17:56:26 -0700

    just starting

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,35 @@
 //create general page components
-alert('Are you ready to play!?')
+// alert('Are you ready to play!?') ***
 //press any key to start
 
+
+//create word list
+
+var words = ["rabbit", "banana", "monkey", "totoro","black",""]
+
+     
 //comp needs to pick random word 
+var randomWord = words[Math.floor(Math.random() * words.length)];
+console.log(randomWord);
+
+// assign that currentWord
 
 //display of letter placeholders = to the number of letters in word chosen
+var answers = [];
+for (var i = 0; i < randomWord.length; i++) {
+    answers[i] = "_";
+}
+console.log(answers.join(' '))
+
+//get user's guess 
+//Check if guess is right
+//if right push to right array
+//if wrong push to wrong array
+
 
 //when keyUp(?) selected key is compared to letters in randomWord and
 
-    //IF letter is true or ===, that letter replaces the placeholder.
+    //IF letter is true or ===, that letter replaces the placeholder, and runs foo.join(' ')
     //ELSE then placed under as letters already guessed.
 
 //Define number of max guesses.    
@@ -16,3 +37,6 @@ alert('Are you ready to play!?')
     //If word is guessed before maxGuesses then .textContent YOU WIN! then game reset.
 
     //If max guesses reached, then .textContext LOSER. Game reset. 
+
+
+    var remainingLetters = randomWord.length;
